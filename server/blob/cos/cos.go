2ca6269ae494aaace71ffd12f672624379c9bf3b
Alan <yumengfei1234@163.com>
2022-03-13 22:22:15 +0800

    cos

--- server/blob/cos/cos.go
+++ server/blob/cos/cos.go
@@ -1,8 +1,12 @@
 package cos
 
 import (
+	"context"
+	"fmt"
+	"io"
 	"net/http"
 	"net/url"
+	"time"
 
 	"github.com/tencentyun/cos-go-sdk-v5"
 )
@@ -13,21 +17,35 @@ type Service struct {
 	secKey string
 }
 
-func NewService() (*Service, error) {
-	u, err := url.Parse("https://coolcar-1253590403.cos.ap-shanghai.myqcloud.com")
+// "https://coolcar-1253590403.cos.ap-shanghai.myqcloud.com"
+
+// "AKIDykGVUDfqkNjocCVXIl4B4h4B1atvgANK"
+// "i9JDrEMHwMWQ5UdWAXeV7TaR2LRHrCH6"
+func NewService(addr, secID, secKey string) (*Service, error) {
+	u, err := url.Parse(addr)
 	if err != nil {
-		panic(err)
+		return nil, fmt.Errorf("cannot parse addr: %v", err)
 	}
 	b := &cos.BaseURL{
 		BucketURL: u,
 	}
-	secID := ""
-	secKey := ""
 
-	cos.NewClient(b, &http.Client{
-		Transport: &cos.AuthorizationTransport{
-			SecretID:  secID,
-			SecretKey: secKey,
-		},
-	})
+	return &Service{
+		client: cos.NewClient(b, &http.Client{
+			Transport: &cos.AuthorizationTransport{
+				SecretID:  secID,
+				SecretKey: secKey,
+			},
+		}),
+		secID:  secID,
+		secKey: secKey,
+	}, nil
+}
+
+func (s *Service) SignURL(c context.Context, method, path string, timeoutSec time.Duration) (string, error) {
+
+}
+
+func (s *Service) Get(c context.Context, path string) (io.ReadCloser, error) {
+
 }
